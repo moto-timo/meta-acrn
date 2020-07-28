8e30b5a7a5c5830f69fe86fcf02e22c84f1d2878
Tim Orling <timothy.t.orling@linux.intel.com>
2020-07-28 10:22:18 -0700

    acrn-efi-setup: begin to generalize the approach
    Currently, the script only works if the rootfs is on /dev/sda
    
    This is very limiting.
    
    Signed-off-by: Tim Orling <timothy.t.orling@linux.intel.com>

--- recipes-core/acrn/acrn-efi-setup/acrn-efi-setup.sh
+++ recipes-core/acrn/acrn-efi-setup/acrn-efi-setup.sh
@@ -11,6 +11,21 @@ for boot in $(efibootmgr | perl -n -e '/Boot([0-9a-fA-F]+).*ACRN.*/ && print "$1
     efibootmgr -b $boot -B
 done
 
+# Where did we boot from?
+# Where is our ESP?
+if [ -d /dev/disk/by-partlabel/msdos ]; then
+    # standard partitions
+    msdos=$(ls -la /dev/disk/by-partlabel/msdos)
+    platform=$(ls -la /dev/disk/by-partlabel/platform)
+else
+    # acrn-image-verified-boot partitions 
+    primary_uefi=$(ls -la /dev/disk/by-partlabel/primary_uefi)
+    secondary_uefi=$(ls -la /dev/disk/by-partlabel/secondary_uefi)
+    rootfs=$(ls -la /dev/disk/by-partlabel/rootfs)
+fi
+
 efibootmgr -c -l "\EFI\BOOT\acrn.efi" \
     -L "ACRN (Yocto)" \
-    -u "bootloader=\EFI\BOOT\bootx64.efi"
+    -d /dev/sdb \
+    -p 1 \
+    -u "bootloader=\EFI\BOOT\bootx64.efi uart=port@0x3f8 "
